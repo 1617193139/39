898a7cdb86d42c244eceb3c02704be40bb62895d
1617193139 <33241903+1617193139@users.noreply.github.com>
2017-11-02 14:54:37 +0800

    Add files via upload

---
+++ 11.2zuoye1.py
@@ -0,0 +1,22 @@
+import smtplib
+import email.mime.multipart
+import email.mime.text
+for num in range(1,10)
+    msg = email.mime.multipart.MIMEMultipart()
+    msg['from'] = '17766472625@163.com'
+    msg['to'] = '1332553880@qq.com'
+    msg['subject'] = 'Lazy'
+    content = "So lazy"
+    txt = email.mime.text.MIMEText(content)
+    msg.attach(txt)
+    smtp = smtplib
+    smtp = smtplib.SMTP()
+    smtp.connect('smtp.163.com', '25')
+    smtp.login('17766472625@163.com', 'zzc123456')
+    smtp.sendmail('17766472625@163.com', '1332553880@qq.com', str(msg))
+    smtp.quit()
+import time
+start_job = True
+while start_job:
+    time,sleep(15)
+
